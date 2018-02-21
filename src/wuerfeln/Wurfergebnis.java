7ab883720161d4f4080f035114214f00ed9dfb57
Reinhard Jebavy <r.jebavy@gmx.at>
2018-02-21 02:05:42 +0100

    Klasse 'Wurfergebnis', neue Methode zaehleAlleBilder(Wuerfel[] wsatz)     siehe 20180221-2230_scrum_activity-log.txt 1.8.2.18) & A5 Notizbuch.     noch nicht getestet!

--- src/wuerfeln/Wurfergebnis.java
+++ src/wuerfeln/Wurfergebnis.java
@@ -24,6 +24,13 @@ public class Wurfergebnis {
 		
 	}
 
+	// zählt die Bilder des übergebenen Würfelsatzes hoch. 
+	public void zaehleAlleBilder(Wuerfel[] wsatz) {
+		for(int w = 0; w < 5; w++){
+			zaehleBilder(wsatz[w].getWert());
+		}
+	}
+	
 	
 
 	
