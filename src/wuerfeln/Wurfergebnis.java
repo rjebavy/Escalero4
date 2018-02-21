7a24c926c99510d1c08efa1c1006f0f7766dbb55
Reinhard Jebavy <r.jebavy@gmx.at>
2018-02-21 03:27:43 +0100

    Klasse 'Wurfergebnis'; bildbezogene Eigenschaften und Methoden gelöscht.      bild1, bild2, bilder1, bilder2 (auch Getter/Setter, toString())      weiteresBild1(int wert), weiteresBild2(int wert)     Aufrufende Methode(n) vergleicheAlle(Wuerfel[] wuerfelsatz) bereits gelöscht.     scrum-sprint 1.8.1) Auswerten; zusätzliche Klasse & Methoden.

--- src/wuerfeln/Wurfergebnis.java
+++ src/wuerfeln/Wurfergebnis.java
@@ -2,10 +2,6 @@ package wuerfeln;
 
 public class Wurfergebnis {
 	private Wuerfel[] wuerfelsatz = new Wuerfel[5];
-	private int bild1; 
-	private int bild2;
-	private int bilder1;
-	private int bilder2;
 	private int gleich1;
 	private int gleich2;
 	private int ungleich;
@@ -99,14 +95,9 @@ public class Wurfergebnis {
 	
 	// alle Auswerteeigenschaften auf 0 setzen. 
 	public void initialisiereAuswerten() {
-		this.setBild1(0);
-		this.setBild2(0);
-		this.setBilder1(0);
-		this.setBilder2(0);
 		this.setGleich1(0);
 		this.setGleich2(0);
 		this.setUngleich(0);
-		this.setNeun(0);
 		this.setNeun(0);
 		this.setAss(0);
 	}
@@ -114,38 +105,6 @@ public class Wurfergebnis {
 	
 
 	// Standard Getter & Setter
-	public int getBild1() {
-		return bild1;
-	}
-	
-	public void setBild1(int bild1) {
-		this.bild1 = bild1;
-	}
-
-	public int getBild2() {
-		return bild2;
-	}
-
-	public void setBild2(int bild2) {
-		this.bild2 = bild2;
-	}
-
-	public int getBilder1() {
-		return bilder1;
-	}
-
-	public void setBilder1(int bilder1) {
-		this.bilder1 = bilder1;
-	}
-
-	public int getBilder2() {
-		return bilder2;
-	}
-
-	public void setBilder2(int bilder2) {
-		this.bilder2 = bilder2;
-	}
-
 	public int getGleich1() {
 		return gleich1;
 	}
@@ -222,8 +181,7 @@ public class Wurfergebnis {
 	// Standard toString
 	@Override
 	public String toString() {
-		return "Wurfergebnis [bild1=" + bild1 + ", bild2=" + bild2 + ", bilder1=" + bilder1 + ", bilder2=" + bilder2
-				+ ", gleich1=" + gleich1 + ", gleich2=" + gleich2 + ", ungleich=" + ungleich + ", neun=" + neun
+		return "Wurfergebnis [gleich1=" + gleich1 + ", gleich2=" + gleich2 + ", ungleich=" + ungleich + ", neun=" + neun
 				+ ", zehn=" + zehn + ", bube=" + bube + ", dame=" + dame + ", koenig=" + koenig + ", ass=" + ass + "]";
 	}
 	
