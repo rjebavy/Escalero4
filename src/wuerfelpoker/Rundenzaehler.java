56211f583aa0c7a451d28a88a6a6b4b6450fde63
Reinhard Jebavy <r.jebavy@gmx.at>
2018-03-10 02:05:21 +0100

    Neue Klasse 'Rundenzaehler', Instanzvariable runden; Sandard Getter/Setter & to String();     neue Methoden initialisiereRundenzaehler() & rundenRunterzaehlen().     siehe 20180309-2217_scrum_activity-log.txt 2.1.2), Klassendiagramm 6 im Projektdokument.     noch nicht getestet!

---
+++ src/wuerfelpoker/Rundenzaehler.java
@@ -0,0 +1,44 @@
+package wuerfelpoker;
+
+public class Rundenzaehler {
+	private int runden; 
+	
+	// Konstruktor 
+	public Rundenzaehler(int runden) {
+		this.runden = runden;
+		}
+
+	
+	// Gehalten Zähler auf 10 setzen; Standardrundenanzahl bei Escalero.  
+	public void initialisiereRundenzaehler() {
+		this.setRunden(10);
+	}
+	
+	//Rundenzähler von 10 auf 0 runterzählen. 
+	public void rundenRunterzaehlen() {
+		if(runden > 0 && runden <= 10) 
+			runden--;
+		else
+			System.out.println("\nRundenzähler außerhalb erlaubten Bereichs (1-10)!");	
+	}
+	
+	
+	
+	
+	// Standard Getter & Setter
+	public int getRunden() {
+		return runden;
+	}
+
+	public void setRunden(int runden) {
+		this.runden = runden;
+	}
+
+	@Override
+	public String toString() {
+		return "Rundenzaehler [runden=" + runden + "]";
+	}
+	
+	
+
+}
